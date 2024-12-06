93007476ec9bbec3923c236b1356575db9ae34ab
Muhammad Bilal <bilalawankamraaa@gmail.com>
2024-12-07 03:18:33 +0500

    feat: add jenkins

---
+++ cypress/e2e/beers.cy.js
@@ -0,0 +1,16 @@
+describe('API Automation - Health Professions Endpoint', () => {
+    it('Validates GET request for professions', () => {
+      cy.request({
+        method: 'GET',
+        url: 'https://api.sampleapis.com/beers/ale', //Endpoint
+      }).then((response) => {
+  
+        expect(response.status).to.eq(200);
+        expect(response.headers['server']).to.include('cloudflare');
+  
+        expect(response.body).to.be.an('array');
+        expect(response.body.length).to.be.greaterThan(0);
+      });
+    });
+  });
+  
